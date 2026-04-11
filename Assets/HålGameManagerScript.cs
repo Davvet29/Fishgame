dc2e710e158c62f125da9e69bd93dd594a3cd010
Davvet29 <Trossingdavid@gmail.com>
2026-04-11 14:34:48 +0200

    guh

--- Assets/HålGameManagerScript.cs
+++ Assets/HålGameManagerScript.cs
@@ -4,15 +4,41 @@ public class HålGameManagerScript : Minigame
 {
     PickupScript pickupScript;
 
+    [SerializeField]
+    GameObject square;
+    [SerializeField]
+    GameObject triangle;
+    [SerializeField]
+    GameObject circle;
+
     void Start()
     {
+
         pickupScript = GameObject.Find("TipPoint").GetComponent<PickupScript>();
         pickupScript.MakeHorizontal(true);
+
+        InstanceShape(Random.Range(1,4));
     }
 
     public void gameOver(bool won)
     {
         gameManager.OnGameEnd(won);
+    }
+
+    public void InstanceShape(int shapeNumber)
+    {
+        if(shapeNumber == 1)
+        {
+
+        }
+        else if (shapeNumber == 2)
+        {
+            
+        }
+        else
+        {
+            
+        }
     }
 
     public HålGameManagerScript(float timeMultiplier, GameManager gameManager)
