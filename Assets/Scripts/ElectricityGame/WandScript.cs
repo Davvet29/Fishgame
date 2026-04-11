86c7c0e7175386a7069e0d5d296db8aef90a584f
Rasmusmario123 <84456427+Rasmusmario123@users.noreply.github.com>
2026-04-11 20:09:19 +0200

    f

--- Assets/Scripts/ElectricityGame/WandScript.cs
+++ Assets/Scripts/ElectricityGame/WandScript.cs
@@ -8,7 +8,7 @@ public class WandScript : MonoBehaviour
     void Start()
     {
         electricityGameManager = GameObject
-            .Find("ElectricityGameManager")
+            .Find("Electricity")
             .GetComponent<ElectricityGameManagerScript>();
     }
 
