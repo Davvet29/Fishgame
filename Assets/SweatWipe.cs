0d023e0a2e60104aeb96af6c3190d19ab57a19e0
Rasmusmario123 <84456427+Rasmusmario123@users.noreply.github.com>
2026-04-11 23:30:27 +0200

    sweat fixed

--- Assets/SweatWipe.cs
+++ Assets/SweatWipe.cs
@@ -6,12 +6,13 @@ public class SweatWipe : Minigame
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     protected override void Start()
     {
-        
+        base.Start();
     }
 
     // Update is called once per frame
     protected override void Update()
     {
+        base.Update();
         if (sweat.sweatWiped)
         {
             gameWon = true;
